98d345f07f8942f6661c78a026fe0f3bd699a032
Calvin Jay Ross <calvinjayross@gmail.com>
2020-08-26 06:30:03 -0500

    added full text search

--- priv/bootstrap.exs
+++ priv/bootstrap.exs
@@ -1,5 +1,9 @@
 alias Grdbii.{Metric, Repo, Python}
 
+Ecto.Adapters.SQL.query!(Repo, """
+  CREATE INDEX IF NOT EXISTS metrics_idx ON metrics USING GIN (to_tsvector('english', name))
+  """)
+
 Python.fetch(:python)
 |> Stream.map(&Metric.from_python/1)
 |> Enum.map(&Repo.insert!/1)
